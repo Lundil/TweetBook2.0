c5c00341eeed06267319dede04699eaecb242bdc
Xavier LAMARQUE <xavier.lamarque@etudiant.univ-lille1.fr>
2017-01-31 08:46:05 +0100

    content

---
+++ WEB-INF/classes/tools/deconnexion.java
@@ -0,0 +1,22 @@
+package tools;
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.util.ArrayList;
+
+/**	servlet gérant l'authentification ou l'inscription d'un utilisateur */
+
+@WebServlet("/deconnexion")
+public class deconnexion extends HttpServlet
+{
+	public void service( HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
+	{
+		//Récupère session actuelle
+    	HttpSession session = request.getSession();
+    	//Kill la session
+    	session.invalidate();
+    	//Redirige vers la page d'acceuil
+    	response.sendRedirect("./index.html");
+    }
+}
